eacb0151a0815fc3566af2240fd437c91ddfefca
JuanDG <juandiaz1@colegioguanenta.edu.co>
2022-04-21 17:43:09 -0500

    desarrollo: creacion diagrama de flujo

---
+++ temperatura.py
@@ -0,0 +1,10 @@
+"--este programaes para calcular los grados a farenheit y kelvin--"
+#input
+c=int(input("digite los datos="))
+
+#processin
+k=c+273.15
+f=1.8*c+32
+
+#ouput
+print(str(c)+" grados centigrados a kelvin serian "+str(k)+" grados y a farenheit serian unos "+str(f))
